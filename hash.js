0c31454a74df7284bc27756a7239922fc9785a0b
Pavel Shevtsov <pelagusrem@gmail.com>
2022-03-24 15:12:51 +0300

    Created js implementation

---
+++ hash.js
@@ -0,0 +1,11 @@
+async function hash(input, algorythm = 'SHA-256') {
+  const inputBuffer = new TextEncoder().encode(input);
+  const hashBuffer = await crypto.subtle.digest(algorythm, inputBuffer)
+  const hashView = new DataView(hashBuffer)
+
+  let result = ''
+  for (let i = 0; i < hashBuffer.byteLength; i += 4) {
+    result += ('00000000' + hashView.getUint32(i).toString(16)).slice(-8)
+  }
+  return result
+}
